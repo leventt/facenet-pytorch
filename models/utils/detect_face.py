46c48f572d54b8c9a3ef3593fd817ff6bc82750f
Ahmet Levent Tasel <leventtasel@gmail.com>
2023-12-25 16:42:35 -0800

    progress for pytorch mediapipe iris detection conversion but there may be a bug as the results aren't as good as the TFLite version

--- models/utils/detect_face.py
+++ models/utils/detect_face.py
@@ -373,6 +373,6 @@ def extract_face(img, box, image_size=160, margin=0, save_path=None):
         os.makedirs(os.path.dirname(save_path) + "/", exist_ok=True)
         save_img(face, save_path)
 
-    face = F.to_tensor(np.float32(face))
+    face = face.float()
 
     return face
